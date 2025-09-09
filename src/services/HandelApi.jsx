ebfcf840cebf843742abcfa7b983767aed949c21
HamzaAsif2002 <hamza20021asif@gmail.com>
2025-09-09 14:52:02 +0500

    add .env

--- src/services/HandelApi.jsx
+++ src/services/HandelApi.jsx
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const key = "AIzaSyCDpAFdLopjv6zNcOeYFR6zN80i5wwh9aM";
-
 const api = axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3/",
 });
@@ -14,7 +12,7 @@ export const GetApiData = async (query) => {
         q: query,
         type: "video",
         maxResults: 8,
-        key: key, // replace with your key
+        key: import.meta.env.VITE_YOUTUBE_API_KEY, // replace with your key
       },
     });
     return res.data;
